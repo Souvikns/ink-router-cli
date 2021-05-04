3d469dd52d87b10ca79124e2ea65762ee84a3cff
souvik <souvikde.ns@gmail.com>
2021-05-04 11:07:25 +0530

    Update error.spec.tsx

--- lib/components/__tests__/error.spec.tsx
+++ lib/components/__tests__/error.spec.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import Error from '../error';
 import { render } from 'ink-testing-library';
-import chalk from 'chalk';
 
 describe('Error Component', () => {
     it("should render bright red error message", () => {
         let { lastFrame } = render(<Error message="Missing Flags" />)
-        expect(lastFrame()).toMatch(chalk`{redBright Missing Flags}`);
+        expect(lastFrame()).toMatch(/Missing Flags/);
     })
 })
