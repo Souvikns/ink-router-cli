f18c545d3869a4dbc4a09cfe5dcfff639454aa16
Souvik <souvikde.ns@gmail.com>
2021-05-31 16:56:13 +0530

    updated doc

--- lib/flags.tsx
+++ lib/flags.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { FlagProps } from './types';
 
+/**
+ * 
+ * @param props 
+ * ```typescript
+ * <Flag name="help" alias="h" />
+ * ``` 
+ */
 export const Flag = (props: FlagProps) => {
     if (props.component) {
         return props.component
